a07682a78d658c39a7a5ddeb42811dcfa9edd810
Andres Felipe Cabezas Quicano <afcabezasq@unal.edu.co>
2023-09-14 21:02:42 -0400

    ShortPAth algorithm

--- LINQ/ProblemShortPath.cs
+++ LINQ/ProblemShortPath.cs
@@ -5,67 +5,103 @@ namespace problem
     public class Node
     {
         public int[] position {get;set;}
-        public List<int[]> track {get;set;};
+        public List<int[]> track {get;set;}
 
         public Node(int[] startingPosition) {
+            track = new List<int[]>();
             position = startingPosition;
-            track.Add(startingPosition);
+        }
+        public override string ToString(){
+            return $"{position[0]},{position[1]}";
         }
     }
     public class Problem
     {
         int[][] grid = {
-            {0,0,0,0,0,0,0},
-            {0,1,0,0,0,0,0},
-            {0,1,0,0,1,0,0},
-            {0,1,0,0,1,0,0},
-            {0,0,0,0,1,0,0},
-            {0,0,0,0,1,0,0},
+           new int[] {0,0,0,0,0,0,0},
+           new int[] {0,1,0,0,0,0,0},
+           new int[] {0,1,0,0,1,0,0},
+           new int[] {0,1,0,0,1,0,0},
+           new int[] {0,0,0,0,1,0,0},
+           new int[] {0,0,0,0,1,0,0},
         };
-        int[] startingPos = [0, 0];
-        int[] finalPos = [7, 5]
+        int[] startingPos = {0, 0};
+        int[] finalPos = {5, 5};
+        Dictionary<string,List[int[][]]> trackMap = new Dictionary<string, List[int[][]]>();
 
 
         int[][] posibleSteps = {
-            {1,1},
-            {1,0},
-            {1,-1},
-            {0,1},
-            {0,-1},
-            {-1,1},
-            {-1,0},
-            {-1,-1},
+           new int[] {1,1},
+           new int[] {1,0},
+           new int[] {1,-1},
+           new int[] {0,1},
+           new int[] {0,-1},
+           new int[] {-1,1},
+           new int[] {-1,0},
+           new int[] {-1,-1},
+        };
+
+        public List<int[]> shortPath(){
+            Dictionary<string, int[][]> track = new Dictionary<string, int[][]>();//[[0,1],[2,3]]
+            HashSet<string> visited = new HashSet<string>();//"3,4"
+            Queue<Node> queue = new Queue<Node>(); //bfs
+        
+            Node start = new Node(this.startingPos);
+            queue.Enqueue(start);
+            while(queue.Count != 0){
+                Node currentNode = queue.Dequeue();
+                visited.Add(currentNode.ToString());
+                trackMap.Add(newNode.ToString(), newNode.track);
+                //Console.WriteLine($"{currentNode.position[0]}, {currentNode.position[1]}");
+                foreach (var step in posibleSteps)
+                {
+                    int[] potentialStep = {step[0]+currentNode.position[0],step[1]+currentNode.position[1]};
+                    Node newNode = new Node(potentialStep);
+                    if(potentialStep[0] >= 0 && potentialStep[1] >= 0 
+                        && potentialStep[0] < grid.Length 
+                        && potentialStep[1] < grid[0].Length
+                        && grid[potentialStep[0]][potentialStep[1]] == 0 
+                        && !visited.Contains(newNode.ToString())) {
+                            
+                            newNode.track = currentNode.track;
+                            
+                            newNode.track.Add(currentNode.position);
+                            queue.Enqueue(newNode);
+                            visited.Add(newNode.ToString());
+                            
+                            if(newNode.position[0] == finalPos[0] && newNode.position[1] == finalPos[1]) {
+                                return newNode.track;
+                            }
+                    }
+                    
+                }
+            }
+            return new List<int[]>(){ 
+                new int[]{-1,-1}
             };
 
-        public int[] shortPath(){
-            HashMap<string, int[][]> track = new HashMap<string, int[][]>();//[[0,1],[2,3]]
-        HashSet<string> visited = new HashSet<string>();//"3,4"
-        Queue<Node> queue = new Queue<Node>(); //bfs
-        
-        Node start = new Node([0,0])
-        queue.Enqueue(start);
-        while(queue.Count != 0){
-            Node currentNode = queue.Dequeue();
-            foreach (var step in posibleSteps)
-            {
-                int[] potentialStep = {step[0]+currentNode.position[0],step[1]+currentNode.position[1]};
-                if(potentialStep[0] >= 0 && potentialStep[1] >= 0 
-                    && potentialStep[1] < grid.length 
-                    && potentialStep[0] < grid[0].length
-                    && grid[potentialStep[0]][potentialStep[1]] == 0) {
-                        Node newNode = new Node(potentialStep);
-                        newNode.track.Add(currentNode.position);
-                        queue.Enqueue(newNode);
-                        if(newNode.position == finalPos) {
-                            return newNode.track;
-                        }
-                }
-                
+        }
+
+        public void displayShortestPath() {
+             List<int[]> solution = this.shortPath();
+            foreach (var pos in solution) {
+                grid[pos[0]][pos[1]] = 2;
             }
-        }
+            foreach (var row in solution) {
+                 foreach (var pos in row) {
+                    Console.Write(pos);
+                 }
+                 Console.WriteLine();
+             }
+
+            foreach (var row in grid) {
+                foreach (var pos in row) {
+                    Console.Write(pos + "  "); 
+                }
+                Console.WriteLine();
+            }
 
         }
-
         
 
     }
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -3,41 +3,45 @@ using Dapper;
 using practiceDapper;
 using System.Data;
 using System.Data.SqlClient;
+using problem;
 
-LinqQueries collectionQueries = new LinqQueries();
+// LinqQueries collectionQueries = new LinqQueries();
 
+Problem p = new Problem();
 
+p.displayShortestPath();
 
-void PrintValues(IEnumerable<Book> booksList)
-{
-    Console.WriteLine("{0,-70} {1,7} {2,11}\n", "Title", "N. Pages", "Publish Date");
-    foreach (var item in booksList)
-    {
-        Console.WriteLine("{0,-70} {1,7} {2,11}\n", item.Title, item.PageCount, item.PublishedDate);
-
-    }
-
-}
-
-// using (IDbConnection connection = new SqlConnection(
-//     "Data Source=.\\;User=myUser;Password=user;Database=AdventureWorks2022;TrustServerCertificate=True"))
+// void PrintValues(IEnumerable<Book> booksList)
 // {
-//     var people = connection.Query<Person>("select * from  Person.Person"); // IENumerable<Person> linq
-//     var peoplev2 = await connection.QueryAsync<Person>("select * from Person.Person");
-//     foreach(var p in peoplev2){
-//         Console.WriteLine($"Hello {p.FirstName}");
+//     Console.WriteLine("{0,-70} {1,7} {2,11}\n", "Title", "N. Pages", "Publish Date");
+//     foreach (var item in booksList)
+//     {
+//         Console.WriteLine("{0,-70} {1,7} {2,11}\n", item.Title, item.PageCount, item.PublishedDate);
+
 //     }
-    
+
 // }
 
-//Print whole collection
-// PrintValues(collectionQueries.WholeCollection());
+// // using (IDbConnection connection = new SqlConnection(
+// //     "Data Source=.\\;User=myUser;Password=user;Database=AdventureWorks2022;TrustServerCertificate=True"))
+// // {
+// //     var people = connection.Query<Person>("select * from  Person.Person"); // IENumerable<Person> linq
+// //     var peoplev2 = await connection.QueryAsync<Person>("select * from Person.Person");
+// //     foreach(var p in peoplev2){
+// //         Console.WriteLine($"Hello {p.FirstName}");
+// //     }
+    
+// // }
 
-// Print books more than 250 pages
-PrintValues(collectionQueries.BooksMoreThan250Pages("Java"));
+// //Print whole collection
+// // PrintValues(collectionQueries.WholeCollection());
+
+// // Print books more than 250 pages
+// PrintValues(collectionQueries.BooksMoreThan250Pages("Java"));
 
 // Print if all the books have status
 // Console.WriteLine(collectionQueries.AllBooksHasStatus());
 //Console.WriteLine(collectionQueries.IsThereAPublishedBookIn2005());
 // PrintValues(collectionQueries.BooksCategoryJavaTop5Skip2());
 // PrintValues(collectionQueries.BooksTitlesNotAvaible());
+
